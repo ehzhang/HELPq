54f48ca5a2e0f74f20a434d07fbd3ddc49b15ff4
Edwin Zhang <ehzhang@mit.edu>
2015-02-20 23:43:11 -0500

    add desktop notifications

--- client/notifications.js
+++ client/notifications.js
@@ -2,16 +2,9 @@
 var intervalId;
 var timeoutId;
 Tracker.autorun(function(){
+
   clearInterval(intervalId);
-
   setDocumentTitle(Q_NAME);
-
-  if (authorized.mentor()){
-    var activeCount = Tickets.find({
-      status: 'OPEN'
-    }).count();
-    setDocumentTitle("\u0020(" + activeCount + ") " + Q_NAME);
-  }
 
   if (authorized.user()){
     var claimedTicket = Tickets.findOne({
@@ -19,16 +12,56 @@ Tracker.autorun(function(){
       status: 'CLAIMED'
     });
     if (claimedTicket){
+      var message = claimedTicket.claimName + " claimed your ticket!";
+
+      // Desktop Notification
+      notifyDesktop(message);
+
       intervalId = setInterval(function(){
-        setDocumentTitle("\u0020(\u2713) " + Q_NAME);
+        setDocumentTitle("(\u2713) " + Q_NAME);
         timeoutId = setTimeout(function(){
-          setDocumentTitle(claimedTicket.claimName + " claimed your ticket!");
+          setDocumentTitle(message);
         },1500)
       },3000);
+    } else {
+      setDocumentTitle(Q_NAME);
     }
+  }
+
+});
+
+Tracker.autorun(function(){
+  if (authorized.mentor()){
+    var activeCount = Tickets.find({
+      status: 'OPEN'
+    }).count();
+    setDocumentTitle("(" + activeCount + ") " + Q_NAME);
   }
 });
 
 function setDocumentTitle(text){
   document.title = text;
+}
+
+
+// Desktop Notifications - EXPERIMENTAL
+Tracker.autorun(function(){
+  if (authorized.user()){
+    // Check to see if the browser supports Notifications
+    if (window["Notification"]){
+      // Request permission
+      Notification.requestPermission();
+    }
+  }
+});
+
+function notifyDesktop(body){
+  if (window["Notification"]){
+    if (Notification.permission == "granted"){
+      var notification = new Notification(Q_NAME, {
+        icon: NOTIFICATION_ICON,
+        body: body
+      });
+    }
+  }
 }
