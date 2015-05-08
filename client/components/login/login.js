9e2f01f67f30411c61dbd4a23eb1dbdaaf7b0b25
Edwin Zhang <ehzhang@mit.edu>
2015-05-08 03:08:00 -0400

    login with password on splash

--- client/components/login/login.js
+++ client/components/login/login.js
@@ -1,3 +1,7 @@
+Template.login.onCreated(function(){
+  this.error = new ReactiveVar();
+});
+
 Template.login.events({
   'click #login-github': function(){
     Meteor.loginWithGithub({
@@ -8,19 +12,46 @@ Template.login.events({
     Meteor.loginWithFacebook({
       loginStyle: 'redirect'
     });
+  },
+  'click #login-password': function(e, t){
+    loginPassword(t);
+  },
+  'keyup #password': function(e, t){
+    if (e.keyCode === 13){
+      loginPassword(t);
+    }
   }
 });
 
 Template.login.helpers({
   enabled: function(){
     var services = {};
-    ServiceConfiguration.configurations.find({}).fetch().forEach(function(service){
-      services[service.service] = true;
-    });
+    ServiceConfiguration.configurations
+        .find({})
+        .fetch()
+        .forEach(function(service){
+          services[service.service] = true;
+        });
     return services;
+  },
+  error: function(){
+    return Template.instance().error.get();
   }
 });
 
 Template.login.rendered = function(){
   $(this.findAll('.container')).addClass('animated fadeIn');
 };
+
+function loginPassword(t){
+  Meteor.loginWithPassword(
+      $(t.findAll('#username')).val(),
+      $(t.findAll('#password')).val(),
+      function(error){
+        if (error){
+          $(t.findAll('#password')).val("");
+          t.error.set(error.reason);
+        }
+      }
+  )
+}
