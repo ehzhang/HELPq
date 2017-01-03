4eec54932c14ba0e60dc807630b9005d70c408cb
Anthony Liu <tlrnalra@gmail.com>
2017-01-03 01:35:46 -0500

    adds a login with CAS option

--- client/components/login/login.js
+++ client/components/login/login.js
@@ -1,57 +1,92 @@
-Template.login.onCreated(function(){
+Template.login.onCreated(function () {
   this.error = new ReactiveVar();
 });
 
 Template.login.events({
-  'click #login-github': function(){
+  'click #login-github': function () {
     Meteor.loginWithGithub({
       loginStyle: 'redirect'
     });
   },
-  'click #login-facebook': function(){
+  'click #login-facebook': function () {
     Meteor.loginWithFacebook({
       loginStyle: 'redirect'
     });
   },
-  'click #login-password': function(e, t){
+  'click #login-password': function (e, t) {
     loginPassword(t);
   },
-  'keyup #password': function(e, t){
-    if (e.keyCode === 13){
+  'click #login-cas': function (e, t) {
+    loginWithCAS({
+      loginStyle: 'redirect'
+    })
+  },
+  'keyup #password': function (e, t) {
+    if (e.keyCode === 13) {
       loginPassword(t);
     }
   }
 });
 
 Template.login.helpers({
-  enabled: function(){
+  enabled: function () {
     var services = {};
     ServiceConfiguration.configurations
         .find({})
         .fetch()
-        .forEach(function(service){
+        .forEach(function (service) {
           services[service.service] = true;
         });
     return services;
   },
-  error: function(){
+  error: function () {
     return Template.instance().error.get();
   }
 });
 
-Template.login.rendered = function(){
+Template.login.rendered = function () {
   $(this.findAll('.container')).addClass('animated fadeIn');
 };
 
-function loginPassword(t){
+function loginPassword(t) {
   Meteor.loginWithPassword(
       $(t.findAll('#username')).val(),
       $(t.findAll('#password')).val(),
-      function(error){
-        if (error){
+      function (error) {
+        if (error) {
           $(t.findAll('#password')).val("");
           t.error.set(error.reason);
         }
       }
   )
+}
+
+ function loginWithCAS (options, callback) {
+  // support both (options, callback) and (callback).
+  if (!callback && typeof options === 'function') {
+    callback = options;
+    options = {};
+  }
+  
+  var config = ServiceConfiguration.configurations.findOne({
+    service: 'cas'
+  });
+  if (!config) {
+    throw new ServiceConfiguration.ConfigError();
+  }
+  
+  var credentialToken = Random.secret();
+  var loginUrl = config.loginURL + "?service=" + Meteor.absoluteUrl('_cas/') + credentialToken;
+  
+  // TODO: add option for popup vs redirect
+  // if(options.loginStyle=='popup') {
+  //
+  // } else if(options.longinStyle=='redirect') {
+  //
+  // }
+  Reload._onMigrate('oauth', function () {
+    return [true, {loginService: 'cas', credentialToken: credentialToken}];
+  });
+  Reload._migrate(null, {immediateMigration: true});
+  //window.location = loginUrl;
 }
--- server/startup.js
+++ server/startup.js
@@ -1,42 +1,43 @@
 // Startup Functions
-Meteor.startup(function(){
+Meteor.startup(function () {
   // Grab the config
   var config = JSON.parse(Assets.getText('config.json'));
-
+  
   // Create the admin
   createAdmin(config.admin.username, config.admin.password);
-
+  
   // Clear Service integrations
   ServiceConfiguration.configurations.remove({});
-
+  
   // Add Service Integrations
   addServiceIntegration('github', config.github);
   addFacebookIntegration(config.facebook);
   addServiceIntegration('google', config.google);
-
+  addCustomIntegration(config.cas);
+  
   // Add Base Settings
   setBasicSettings(config);
-
-  Accounts.onCreateUser(function(options, user){
-    if (options.profile){
+  
+  Accounts.onCreateUser(function (options, user) {
+    if (options.profile) {
       user.profile = options.profile;
-
-      if (config.defaultMentor){
+      
+      if (config.defaultMentor) {
         user.profile.mentor = true;
       }
     }
-
+    
     return user;
   });
-
+  
 });
 
-function createAdmin(username, password){
+function createAdmin(username, password) {
   var user = Meteor.users.findOne({
     username: username
   });
-
-  if (!user){
+  
+  if (!user) {
     Accounts.createUser({
       username: username,
       password: password,
@@ -45,22 +46,21 @@ function createAdmin(username, password){
       }
     });
   }
-
+  
   Meteor.users.update({
     username: username
-  },{
-    $set:
-      {
-        'profile.admin': true
-      }
+  }, {
+    $set: {
+      'profile.admin': true
+    }
   })
 }
 
-function addServiceIntegration(service, config){
-  if (config.enable){
+function addServiceIntegration(service, config) {
+  if (config.enable) {
     ServiceConfiguration.configurations.upsert({
       service: service
-    },{
+    }, {
       $set: {
         clientId: config.clientId,
         secret: config.secret
@@ -69,11 +69,24 @@ function addServiceIntegration(service, config){
   }
 }
 
-function addFacebookIntegration(fb){
-  if (fb.enable){
+function addCustomIntegration(cas) {
+  if (cas.enable) {
+    ServiceConfiguration.configurations.upsert({
+      service: 'cas'
+    }, {
+      $set: {
+        baseURL: cas.baseURL,
+        loginURL: cas.loginURL
+      }
+    })
+  }
+}
+
+function addFacebookIntegration(fb) {
+  if (fb.enable) {
     ServiceConfiguration.configurations.upsert({
       service: 'facebook'
-    },{
+    }, {
       $set: {
         appId: fb.appId,
         secret: fb.secret
@@ -82,10 +95,10 @@ function addFacebookIntegration(fb){
   }
 }
 
-function setBasicSettings(config){
+function setBasicSettings(config) {
   // Check if the settings document already exists
   var settings = Settings.find({}).fetch();
-  if (settings.length == 0 || settings.length > 1){
+  if (settings.length == 0 || settings.length > 1) {
     // Remove all documents and then create the singular settings document.
     Settings.remove({});
     Settings.insert(config.settings);
