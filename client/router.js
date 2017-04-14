803dcd7a1af4525c43669c2024a901ee5f2e86e4
Gerard Casas Saez <casassg@gmail.com>
2017-04-13 21:04:26 -0600

    add special url for mentors

--- client/router.js
+++ client/router.js
@@ -11,6 +11,22 @@ Router.route('/', function(){
   this.render('home');
 });
 
+Router.route('/join/mentors', function(){
+    var profile = {
+        'mentor': 'true'
+    };
+
+    Meteor.call("updateUser", Meteor.userId(), profile, function(err){
+        if (err){
+            console.error(err)
+        } else {
+            console.info('Successfully converted to mentor!')
+        }
+    });
+    this.redirect('/')
+});
+
+
 Router.route('/profile', function(){
   this.layout('bannerLayout');
   this.render('profile');
