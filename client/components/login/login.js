3d962ea4ec835fb0577cdf916b8ae570f6d418ce
mjrb <www.w.com123@gmail.com>
2018-07-15 23:33:11 -0400

    change frontend to use LCSLoginCheck method to auto sign up users from lcs that don't exist yet

--- client/components/login/login.js
+++ client/components/login/login.js
@@ -44,14 +44,29 @@ Template.login.rendered = function(){
 };
 
 function loginPassword(t){
-  Meteor.loginWithPassword(
-      $(t.findAll('#username')).val().trim(),
-      $(t.findAll('#password')).val(),
-      function(error){
-        if (error){
-          $(t.findAll('#password')).val("");
-          t.error.set(error.reason);
-        }
-      }
-  )
+  const username=$(t.findAll('#username')).val().trim();
+  const password=$(t.findAll('#password')).val();
+  // TODO remove all these log statements after debugging is done
+  function loginCallback(callbackError, existsInLCS){
+    console.log(callbackError,existsInLCS)
+    if(existsInLCS){
+      Meteor.loginWithPassword(
+	username,
+	password,
+	function(error){
+          if (error){
+            $(t.findAll('#password')).val("");
+	    // TODO
+	    // for some reason login fails here and says that the user doesn't exist
+	    // even tho the user looks like its getting properly inserted into the db
+	    // and it is a valid lcs user.
+	    console.log("inner",username)
+            t.error.set(error.reason);
+          }
+	})
+    }else{
+      t.error.set("Bad username or password");
+    }
+  }
+  Meteor.call('LCSLoginCheck',username, password, loginCallback);
 }
