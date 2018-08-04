83661302eb93777a4e3e99bceb629a85b8a3e25d
mjrb <www.w.com123@gmail.com>
2018-08-04 14:46:12 -0400

    supposedly implement password updating

--- server/lib/lcs.js
+++ server/lib/lcs.js
@@ -1,38 +1,62 @@
 // parameterized login function to be initialized from setup.js
 // with the config
 Meteor.addLCSLogin = function(config){
-  Meteor.methods({
-    LCSLoginCheck: function(username, password){
-      check(username, String);
-      check(password, String);
-
-      if(!Meteor.users.findOne({username:username})){
-	const LCSResponse = HTTP.call('POST', config.authURL, {
-	  data: {
-	    email: username,
-	    password: password
-	  }
-	});
-	if(LCSResponse.data.statusCode !== 200){
-	  return false;
-	}
-	const responseBody = LCSResponse.data.body;
-	const token = JSON.parse(responseBody).auth.token;
-	// TODO update roles if marked as mentor in lcs
-	// TODO update user password if changed in lcs
-	//it is important to add email an username because
-	//if there is no email attached to a user, then when the
-	//client tries to use Meteor.loginWithPassword
-	//it will see that you're trying to use an email (because it has an @)
-	//and it will fail because no user has that email
-	Accounts.createUser({
-	  username: username,
-	  password: password,
-	  email: username,
-	  profile: {name:username}
-	});
+  const getLCSToken = function(username, password){
+    console.log(config.URL+"/authorize");
+    (console.log(username, password))
+    const LCSResponse = HTTP.call("POST", config.URL+"/authorize", {
+      data: {
+	email: username,
+	password: password
       }
-      return true;
+    });
+    console.log(LCSResponse)
+    if(LCSResponse.data.statusCode !== 200){
+      return null;
     }
+    const responseBody = LCSResponse.data.body;
+    return JSON.parse(responseBody).auth.token;
+  }
+  const isLCSMentor = function(email, token){
+    const userData = HTTP.call("POST", config.URL+"/read", {
+      data: {
+	email: email,
+	token: token,
+	//looking for a user document with our email will give
+	//us the entire user's document
+	query: {email: email}
+      }
+    });
+    return userData.role.mentor
+  }
+  const LCSLoginCheck = function(username, password){
+    check(username, String);
+    check(password, String);
+    const LCSToken = getLCSToken(username, password)
+    const userInDB = Meteor.users.findOne({username: username});
+    
+    // TODO update roles if marked as mentor in lcs
+    // TODO update user password if changed in lcs
+    if(!userInDB && !LCSToken){
+      return false;
+    }else if(!userInDB && LCSToken){
+      //it is important to add email an username because
+      //if there is no email attached to a user, then when the
+      //client tries to use Meteor.loginWithPassword
+      //it will see that you're trying to use an email (because it has an @)
+      //and it will fail because no user has that email
+      Accounts.createUser({
+	username: username,
+	password: password,
+	email: username,
+	profile: {name:username}
+      });
+    }else if(userInDB && LCSToken){
+      Accounts.setPassword(userInDB._id, password)
+    }
+    return true;
+  }
+  Meteor.methods({
+    LCSLoginCheck: LCSLoginCheck
   });
 }
